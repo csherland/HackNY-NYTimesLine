f8f0e2da06ec59aa6452f259d05984d9f4cc549d
Christian Sherland <christiansherland@gmail.com>
2013-09-29 01:32:47 -0400

    yo.

--- nytimesline/semanticSearch/models.py
+++ nytimesline/semanticSearch/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Article(model.Model):
+class Article(models.Model):
     date = models.DateTimeField()
     title = models.SlugField(max_length=255)
     picURL = models.URLField(max_length=511)
     description = models.TextField()
 
-class Timeline(model.Model):
+class Timeline(models.Model):
     query = models.CharField(max_length=255)
     articles = []
