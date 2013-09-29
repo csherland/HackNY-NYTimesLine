1677fa90c9f1bcee7b0505036cee82fa0cca848e
Christian Sherland <christiansherland@gmail.com>
2013-09-28 23:39:45 -0400

    fixing broken stuff

--- nytimesline/semanticSearch/models.py
+++ nytimesline/semanticSearch/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Article(model.Models):
+class Article(model.Model):
     date = models.DateTimeField()
     title = models.SlugField(max_length=255)
     picURL = models.URLField(max_length=511)
     description = models.TextField()
 
-class Timeline(models.Model):
+class Timeline(model.Model):
     query = models.CharField(max_length=255)
     articles = []
