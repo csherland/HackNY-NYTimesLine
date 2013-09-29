bf601bb405f6345c16164a973f3990259bae2e93
SharangP <sharang.phadke@gmail.com>
2013-09-29 02:09:12 -0400

    christian is a fag. fixed url queries by making spaces %20s

--- nytimesline/semanticSearch/SemanticService.py
+++ nytimesline/semanticSearch/SemanticService.py
@@ -1,4 +1,5 @@
 import json
+import urllib
 import urllib2
 import random
 
@@ -7,13 +8,16 @@ class SemanticService:
 
     __searchUrl__ = "http://api.nytimes.com/svc/semantic/v2/concept/search.json?query={Query}&api-key={ApiKey}"
     __nameUrl__ = "http://api.nytimes.com/svc/semantic/v2/concept/name/{Type}/{Name}?fields=all&api-key={ApiKey}"
+    __hdr__ = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }
     _Type_ = {
         "location": "nytd_geo",
         "person": "nytd_per",
         "organization": "nytd_org",
         "descriptor": "nytd_des"
     }
-
 
     def __init__(self, key):
         self.__ApiKey__ = key
@@ -26,12 +30,9 @@ class SemanticService:
         """
         results = []
         url = self.__searchUrl__.format(Query=query, ApiKey=self.__ApiKey__)
+        url = url.replace(' ','%20')
         try:
-            hdr = {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-            }
-            req = urllib2.Request(url, headers=hdr)
+            req = urllib2.Request(url, headers=self.__hdr__)
             jsonResponse = urllib2.urlopen(req)
             results = json.load(jsonResponse)["results"]
             return results
@@ -46,12 +47,9 @@ class SemanticService:
         """
         results = []
         url = self.__nameUrl__.format(Type=type, Name=name, ApiKey=self.__ApiKey__)
+        url = url.replace(' ','%20')
         try:
-            hdr = {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-            }
-            req = urllib2.Request(url, headers=hdr)
+            req = urllib2.Request(url, headers=self.__hdr__)
             jsonResponse = urllib2.urlopen(req)
             results = json.load(jsonResponse)["results"]
             return results
