fdb6442699c096320d02776ad354bc5beb8dfe59
SharangP <sharang.phadke@gmail.com>
2013-09-28 22:11:47 -0400

    added model

--- nytimesline/semanticSearch/models.py
+++ nytimesline/semanticSearch/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Article(model.Models):
+    date = models.DateTimeField()
+    title = models.SlugField(max_length=255)
+    picURL = models.URLField(max_length=511)
+    description = models.TextField()
+
+class Timeline(models.Model):
+    query = models.CharField(max_length=255)
+    articles = []
