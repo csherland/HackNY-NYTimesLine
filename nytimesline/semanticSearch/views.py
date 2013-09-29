0930f93403497c16d98d5adac8c8156a0c05cc5f
Christian Sherland <christiansherland@gmail.com>
2013-09-28 21:58:03 -0400

    fixing little thing

--- nytimesline/semanticSearch/views.py
+++ nytimesline/semanticSearch/views.py
@@ -1,6 +1,5 @@
 #Create your views here.
 from django.shortcuts import render, get_object_or_404
-from semanticSearch.models import SemanticService
 
 def index(request):
 	return render(request, 'semanticSearch/index.html',{})
